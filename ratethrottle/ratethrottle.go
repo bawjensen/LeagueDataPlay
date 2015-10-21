6f1d1775959d6c00126d933a44076afe7fe43cb6
Bryan Jensen <bryanawjensen@gmail.com>
2015-10-20 17:00:42 -0700

    Reworking rate throttle to use tick and buffered chan instead of ring buffer

--- ratethrottle/ratethrottle.go
+++ ratethrottle/ratethrottle.go
@@ -1,75 +1,109 @@
 package ratethrottle
 
 import (
-    "fmt"
-    "time"
-    . "github.com/bawjensen/dataplay/utility"
+	"fmt"
+	"time"
+	. "github.com/bawjensen/dataplay/utility"
 )
 
+// ------------------------------------- Global Variables ------------------------------------------
+
+var instance *rateThrottle
+
+// ------------------------------------- Ring Buffer Logic -----------------------------------------
+
 type ringBuffer struct {
-    ring []time.Time
-    size int
-    period time.Duration
-    curr int
+	ring []time.Time
+	size int
+	period time.Duration
+	curr int
 }
 
 func newRingBuffer(size int, period time.Duration) (self ringBuffer) {
-    self = ringBuffer{ring: make([]time.Time, size, size), curr: 0, size: size, period: period}
+	self = ringBuffer{ring: make([]time.Time, size, size), curr: 0, size: size, period: period}
 
-    return self
+	return self
 }
 
 func (self *ringBuffer) increment() {
-    self.curr = (self.curr + 1) % self.size
+	self.curr = (self.curr + 1) % self.size
 }
 
 func (self *ringBuffer) current() time.Time {
-    return self.ring[self.curr]
+	return self.ring[self.curr]
 }
 
 func (self *ringBuffer) setCurrent(newTime time.Time) {
-    self.ring[self.curr] = newTime
+	self.ring[self.curr] = newTime
 }
 
-type RateThrottle struct {
-    buffer ringBuffer
-    wait chan bool
+// ------------------------------------- Rate Throttle Logic ---------------------------------------
+
+type rateThrottle struct {
+	// rate 			int
+	// ratePer 	time.Duration
+	fillInterval	time.Duration
+	// curr 			int
+	// last_check 		time.Time
+	wait 			chan bool
 }
 
-var instance *RateThrottle
+// func newRateThrottle() (self rateThrottle) {
+// 	size := int(REQUEST_CAP / RATE_THROTTLE_GRANULARITY)
+// 	time := ((REQUEST_PERIOD + RATE_THROTTLE_BUFFER) * 1000 * time.Millisecond) / RATE_THROTTLE_GRANULARITY
+// 	fmt.Printf("Initializing rate throttler with size %d and time %v\n", size, time)
+// 	self = rateThrottle{buffer: newRingBuffer(int(size), time), wait: make(chan bool)}
+// 	return
+// }
 
-func init() {
-    if instance != nil {
-        fmt.Println("instance wasn't nil - How is this possible?")
-    }
+func newRateThrottle() (self rateThrottle) {
+	fillInterval := (REQUEST_PERIOD * time.Second) / REQUEST_CAP
+	bufferSize := int(REQUEST_CAP / RATE_THROTTLE_GRANULARITY)
 
-    instance = new(RateThrottle) // Allocate an actual space for non-nil RateThrottle
-    (*instance) = newRateThrottle() // Populate that
+	fmt.Printf("Initializing rate throttler with interval %v and buffer %d\n", fillInterval, bufferSize)
 
-    go func() {
-        for {
-            instance.wait <- true
+	self = rateThrottle{
+		fillInterval: 	fillInterval,
+		wait: 			make(chan bool, bufferSize),
+	}
 
-            instance.buffer.setCurrent(time.Now())
-            instance.buffer.increment()
-
-            lastTime := instance.buffer.current()
-            if timeSince := time.Since(lastTime); (!lastTime.IsZero()) && (timeSince < instance.buffer.period) {
-                // fmt.Printf("At %v, time is %v, sleeping for %v\n", instance.buffer.curr, instance.buffer.ring[instance.buffer.curr].Second(), instance.buffer.period - timeSince)
-                time.Sleep(instance.buffer.period - timeSince)
-            }
-        }
-    }()
+	return
 }
 
-func newRateThrottle() (self RateThrottle) {
-    size := int(REQUEST_CAP / RATE_THROTTLE_GRANULARITY)
-    time := ((REQUEST_PERIOD + RATE_THROTTLE_BUFFER) * 1000 * time.Millisecond) / RATE_THROTTLE_GRANULARITY
-    fmt.Printf("Initializing rate throttler with size %d and time %v\n", size, time)
-    self = RateThrottle{buffer: newRingBuffer(int(size), time), wait: make(chan bool)}
-    return
-}
+// ------------------------------------- Package behavior Logic ------------------------------------
 
 func Wait() {
-    <-instance.wait
+	<-instance.wait
+}
+
+// ------------------------------------- Package Init Logic ----------------------------------------
+
+func init() {
+	if instance != nil {
+		fmt.Println("instance wasn't nil - How is this possible?")
+	}
+
+	instance = new(rateThrottle) 	// Allocate an actual space for non-nil rateThrottle
+	(*instance) = newRateThrottle() // Populate that
+
+	go func() {
+		for _ = range time.Tick(instance.fillInterval) {
+			instance.wait <- true
+		}
+	}()
+
+	// go func() {
+	// 	for {
+	// 		instance.wait <- true
+
+	// 		instance.buffer.setCurrent(time.Now())
+	// 		instance.buffer.increment()
+
+	// 		lastTime := instance.buffer.current()
+	// 		if timeSince := time.Since(lastTime); (!lastTime.IsZero()) && (timeSince < instance.buffer.period) {
+	// 			// fmt.Printf("At %v, time is %v, sleeping for %v\n", instance.buffer.curr, instance.buffer.ring[instance.buffer.curr].Second(), instance.buffer.period - timeSince)
+	// 			time.Sleep(instance.buffer.period - timeSince)
+	// 		}
+	// 	}
+	// }()
 }
